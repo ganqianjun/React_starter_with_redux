74e224355929391f86c5244e7c04f8f31b90f1ef
ganqianjun <ganqianjun@outlook.com>
2017-06-18 12:51:10 -0700

    React starter inlcude: loader for js/css; babel for react; redux; webpack

---
+++ bootstrap.js
@@ -0,0 +1,7 @@
+// customize some setting before start the server
+var open = require('open');
+const SERVER_PORT = process.env.SERVER_PORT || 8080;
+
+require('./node_modules/webpack-dev-server/bin/webpack-dev-server.js');
+
+open(`http://localhost:${SERVER_PORT}`, 'Google Chrome'); //webpack-dev-server default port
